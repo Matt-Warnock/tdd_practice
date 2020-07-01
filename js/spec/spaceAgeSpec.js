dcba4a42b79e0361bc08675e041004deddcae2ce
MattW <drflamerock@yahoo.co.uk>
2020-07-01 15:16:10 +0100

    Add js files

---
+++ js/spec/spaceAgeSpec.js
@@ -0,0 +1,39 @@
+describe('Space Age', () => {
+  let spaceage;
+
+  beforeEach(() => {
+    spaceage = new SpaceAge();
+  });
+
+  it('calculates age on Earth', () => {
+    expect(spaceage.age('earth', 1000000000)).toEqual(31.69);
+  });
+
+  xit('calculates age on Mercury', () => {
+    expect(spaceage.age('mercury', 2134835688)).toEqual(280.88);
+  });
+
+  xit('calculates age on Venus', () => {
+    expect(spaceage.age('venus', 189839836)).toEqual(9.78);
+  });
+
+  xit('calculates age on Mars', () => {
+    expect(spaceage.age('mars', 2129871239)).toEqual(35.88);
+  });
+
+  xit('calculates age on Jupiter', () => {
+    expect(spaceage.age('jupiter', 901876382)).toEqual(2.41);
+  });
+
+  xit('calculates age on Saturn', () => {
+    expect(spaceage.age('saturn', 2000000000)).toEqual(2.15);
+  });
+
+  xit('calculates age on Uranus', () => {
+    expect(spaceage.age('uranus', 1210123456)).toEqual(0.46);
+  });
+
+  xit('calculates age on Neptune', () => {
+    expect(spaceage.age('neptune', 1821023456)).toEqual(0.35);
+  });
+});
---
+++ js/src/spaceAge.js
@@ -0,0 +1,5 @@
+class SpaceAge {
+  age() {
+    
+  }
+}
