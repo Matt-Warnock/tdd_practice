acdf44040ec4acb7519c7991f835bdfb7779a9bc
MattW <drflamerock@yahoo.co.uk>
2020-07-01 13:06:04 +0100

    Pass final test refactor object for readability

--- js/src/transcription.js
+++ js/src/transcription.js
@@ -1,12 +1,23 @@
 class Transcription {
 
   toRNA(dna) {
-    const dna_to_rna = {C: 'G', G: 'C', T: 'A', A: 'U'};
+    const dna_to_rna = {
+      C: 'G',
+      G: 'C',
+      T: 'A',
+      A: 'U'
+    };
+    
     let rna = '';
 
     Array.from(dna).forEach(nucleotide => {
-          rna += dna_to_rna[nucleotide];
-      });
+      if (dna_to_rna[nucleotide] === undefined) {
+        rna += '?';
+        return;
+      }
+
+      rna += dna_to_rna[nucleotide];
+    });
 
     return rna;
   }
