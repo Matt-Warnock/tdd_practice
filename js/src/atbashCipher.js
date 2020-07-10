589681d869a0b2faaf5184024097f97ce3e715fc
MattW <drflamerock@yahoo.co.uk>
2020-07-10 21:49:56 +0100

    Refactored

--- js/src/atbashCipher.js
+++ js/src/atbashCipher.js
@@ -1,11 +1,18 @@
 class AtbashCipher {
-  encode(cipher) {
-    if (cipher === 'no') {
-      return 'ml';
-    }
-    if (cipher === 'OMG') {
-      return 'lnt';
-    }
-    return 'bvh';
+  encode(input) {
+    let encoded = '';
+    let alphabet = Array.from('abcdefghijklmnopqrstuvwxyz');
+    let cipher = alphabet.slice().reverse();
+
+    let message = input.toLowerCase();
+
+    Array.from(message).forEach(character => {
+      alphabet.forEach((letter, index) => {
+        if (character === letter) {
+          encoded += cipher[index];
+        }
+      });
+    });
+    return encoded;
   }
 }
