118b11f4585735f4f5dc5423867097314a670165
MattW <drflamerock@yahoo.co.uk>
2020-07-14 16:15:27 +0100

    Pass next two tests

--- js/spec/hammingDistanceSpec.js
+++ js/spec/hammingDistanceSpec.js
@@ -25,13 +25,13 @@ describe('Hamming', () => {
     expect(hamming.compute('GGACGGATTCTG', 'AGGACGGATTCT')).toEqual(9);
   });
 
-  xit('disallows when first strand is longer', () => {
+  it('disallows when first strand is longer', () => {
     expect(() => hamming.compute('AATG', 'AAA')).toThrow(
       new Error('left and right strands must be of equal length'),
     );
   });
 
-  xit('disallows when second strand is longer', () => {
+  it('disallows when second strand is longer', () => {
     expect(() => hamming.compute('ATA', 'AGTG')).toThrow(
       new Error('left and right strands must be of equal length'),
     );
--- js/src/hammingDistance.js
+++ js/src/hammingDistance.js
@@ -1,5 +1,9 @@
 class Hamming {
   compute(strandA, strandB) {
+    if (strandA.length !== strandB.length) {
+      throw new Error('left and right strands must be of equal length');
+    }
+    
     let hammingDistance = 0;
 
     Array.from(strandA).forEach((dna, i) => {
