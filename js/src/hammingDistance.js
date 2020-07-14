97e0c200ab2248970df9171ed85393d0f7543ee7
MattW <drflamerock@yahoo.co.uk>
2020-07-14 17:30:08 +0100

    Second refactor

--- js/src/hammingDistance.js
+++ js/src/hammingDistance.js
@@ -1,25 +1,26 @@
 class Hamming {
   compute(strandA, strandB) {
     let hammingDistance = 0;
-    const conditions = [strandA.length === 0 && strandB.length > 0,
-                        strandB.length === 0 && strandA.length > 0,
-                        strandA.length !== strandB.length],
+    const conditions = [strandA.length === 0,
+                        strandB.length === 0],
           errorMessages = ['left strand must not be empty',
-                           'right strand must not be empty', 
-                           'left and right strands must be of equal length'];
+                           'right strand must not be empty'];
 
-    for (var i = 0; i < conditions.length; i++) {
-      if (conditions[i]) {
-        throw new Error(errorMessages[i]);
+      if (strandA.length !== strandB.length) {
+        for (var i = 0; i < conditions.length; i++) {
+          if (conditions[i]) {
+            throw new Error(errorMessages[i]);
+          }
+        }
+        throw new Error('left and right strands must be of equal length');
       }
+      
+      Array.from(strandA).forEach((dna, i) => {
+        if (dna !== strandB[i]) {
+          hammingDistance += 1;
+        }
+      });
+
+      return hammingDistance;
     }
-
-    Array.from(strandA).forEach((dna, i) => {
-      if (dna !== strandB[i]) {
-        hammingDistance += 1;
-      }
-    });
-
-    return hammingDistance;
   }
-}
